00eb155220e987552d6c47bd11ca006b7a97adc9
Mo <mohamedjama53@gmail.com>
2019-12-29 19:40:15 +0000

    force node to use 13.2 or higher

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.get("/info", (request, response) => {
   response.send(`<p>${info.data}</p><p>${info.date}</p>`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
